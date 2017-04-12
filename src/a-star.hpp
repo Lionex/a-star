8f131da1d8a6b33f2ba9afa6b175d863a6c12e92
Gwen Lofman <Gwen@Lofman.co>
2017-04-12 18:47:10 -0400

    Add a-star.hpp module description

--- src/a-star.hpp
+++ src/a-star.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
+/*
+
+A* graph search finds the optimal path from some start node in a weighted digraph to an end node provided such a path exists. A* uses a priority queue to ensure the algorithm first searches elements which minimize the traversal cost to the curent node and the value of a heurisic which estimates the future cost from the current node to the destination.
+
+This implementation uses an adjacency matrix to represent a digraph, however, which works best in cases where the adjacency matrix is dense meaning each node in the digraph is connected to many other nodes in the graph.
+
+ */
+
 #include <array>
 #include <deque>
 #include <iostream>
 #include <list>
 #include <unordered_map>
 #include <queue>
-#include <vector>
 #include <utility>
 
 #include "a-star.cpp"
@@ -17,6 +24,10 @@ bool contains(std::deque<Node> queue, Node node);
 template<typename Node, typename Cost, size_t N>
 size_t index_of(Node node, Node nodes[N]);
 
+
+/*
+
+ */
 template<typename Node, typename Cost, size_t N>
 std::list<Node> aStar(
     const Node &START,
